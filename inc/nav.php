75de2edf4d09e21c57780e2b9cacd82fc5cc2225
Bolaji Ayodeji <sirbeejay1@gmail.com>
2018-10-20 11:45:19 +0100

    Add files via upload

--- inc/nav.php
+++ inc/nav.php
@@ -7,167 +7,11 @@
 			</div>
 			<div class="header-right-info pull-right clearfix">
 				<div class="single-header-info">
-					<div class="icon-box">
-						<div class="inner-box">
-							<i class="flaticon-interface-2"></i>
-						</div>
-					</div>
-					<div class="content">
-						<h3>EMAIL</h3>
-						<p>info@technerepublic.org</p>
-					</div>
-				</div>
-				<div class="single-header-info">
-					<div class="icon-box">
-						<div class="inner-box">
-							<i class="flaticon-telephone"></i>
-						</div>
-					</div>
-					<div class="content">
-						<h3>Call Now</h3>
-						<p><b>(+234) 8109445504</b></p>
-					</div>
-				</div>
+					
+					
 				<div class="single-header-info">
 					<!-- Modal: donate now Starts -->
-					<a class="thm-btn" data-toggle="modal" href="#modal-donate-now">Donate Now</a>
-					<div class="modal fade" id="modal-donate-now" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-						<div class="modal-dialog style-one" role="document">
-							<div class="modal-content">
-								<div class="modal-header">
-									<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-									<h4 class="modal-title" id="myModalLabel">Make a Donation</h4>
-								</div>
-								<div class="modal-body">
-									<div class="donation-form-outer">
-										<form action="contact.html" method="post">
-
-											<!--Form Portlet-->
-											<div class="form-portlet">
-												<h3>Donation Amount</h3>
-
-												<div class="row clearfix">
-													<div class="form-group col-xs-12 clearfix">
-
-														<div class="radio-select">
-															<input type="radio" id="amount-1" name="sel-amount">
-															<label for="amount-1">$10</label>
-														</div>
-
-														<div class="radio-select">
-															<input type="radio" checked="" id="amount-2" name="sel-amount">
-															<label for="amount-2">$25</label>
-														</div>
-
-														<div class="radio-select">
-															<input type="radio" id="amount-3" name="sel-amount">
-															<label for="amount-3">$50</label>
-														</div>
-
-														<div class="radio-select">
-															<input type="radio" id="amount-4" name="sel-amount">
-															<label for="amount-4">$100</label>
-														</div>
-
-														<div class="radio-select">
-															<input type="radio" id="amount-5" name="sel-amount">
-															<label for="amount-5">$150</label>
-														</div>
-
-														<div class="radio-select">
-															<input type="radio" id="amount-6" name="sel-amount">
-															<label for="amount-6">$200</label>
-														</div>
-
-														<div class="radio-select">
-															OR
-														</div>
-
-													</div>
-
-													<div class="form-group  col-xs-12 padd-top-20">
-
-														<input type="text" placeholder="Other Amount" value="" name="other-amount">
-
-													</div>
-
-												</div>
-											</div>
-
-											<hr>
-
-											<!--Form Portlet-->
-											<div class="form-portlet">
-												<h3>Billing Information</h3>
-
-												<div class="row clearfix">
-
-													<div class="form-group col-lg-6 col-md-6 col-xs-12">
-														<div class="field-label">Card Number <span class="required">*</span></div>
-														<input type="text" required="" placeholder="Card Number" value="" name="name">
-													</div>
-
-													<div class="form-group col-lg-6 col-md-6 col-xs-12">
-														<div class="field-label">Security Code (CVC) <span class="required">*</span></div>
-														<input type="text" required="" placeholder="Security Code" value="" name="name">
-													</div>
-
-													<div class="form-group col-lg-6 col-md-6 col-xs-12">
-														<div class="field-label">Name <span class="required">*</span></div>
-														<input type="text" required="" placeholder="First Name" value="" name="name">
-													</div>
-
-													<div class="form-group col-lg-6 col-md-6 col-xs-12">
-														<div class="field-label">Email <span class="required">*</span></div>
-														<input type="email" required="" placeholder="Email" value="" name="name">
-													</div>
-
-													<div class="form-group col-lg-6 col-md-6 col-xs-12">
-														<div class="field-label">Phone <span class="required">*</span></div>
-														<input type="text" required="" placeholder="Phone" value="" name="name">
-													</div>
-
-													<div class="form-group col-lg-6 col-md-6 col-xs-12">
-														<div class="field-label">Address <span class="required">*</span></div>
-														<input type="text" required="" placeholder="Address 1" value="" name="name">
-													</div>
-
-													<div class="form-group col-lg-6 col-md-6 col-xs-12">
-														<div class="field-label">City <span class="required">*</span></div>
-														<select>
-															<option>City</option>
-															<option>City Name</option>
-															<option>City Other</option>
-														</select>
-													</div>
-
-													<div class="form-group col-lg-6 col-md-6 col-xs-12">
-														<div class="field-label">Zip / Postal Code <span class="required">*</span></div>
-														<input type="text" required="" placeholder="Zip Code" value="" name="name">
-													</div>
-
-													<hr>
-
-													<div class="text-center"><button class="thm-btn mt_30 mb_30" type="submit">Donate Now</button></div>
-
-												</div>
-											</div>
-
-											<hr>
-
-											<!--Form Portlet-->
-											<div class="form-portlet">
-												<h3>Online Payment Information</h3>
-
-												<div class="payment-option-logo"><img alt="" src="img/resources/payment-logos.png" class="img-responsive"></div>
-												<br>
-											</div>
-
-										</form>
-									</div>
-								</div>
-							</div>
-						</div>
+					<a class="thm-btn" data-toggle="modal" href="https://docs.google.com/forms/d/e/1FAIpQLSf5XKy4MlfYrOnyN3v50uN_FJyxhHznuPTLiwjoLsaEWoJZ_w/viewform?usp=sf_link">Become a member</a>
 					</div>
 					<!-- Modal: donate now Ends -->
 				</div>
@@ -176,7 +20,7 @@
 	</header> <!-- /.header -->
 
 
-	<nav class="mainmenu-area stricky">
+	<nav class="mainmenu-area sticky">
 		<div class="container">
 			<div class="navigation pull-left">
 				<div class="nav-header">
@@ -222,11 +66,11 @@
 							<a href="#">Blog</a>
 						</li>
 						<li>
-						<div class="text-center"><a href="#" class="thm-btn mt_30 mb_30">SIGN UP</a>&nbsp;&nbsp;</div>
+						<div class="text-center"><a href="#" class="thm-btn mt_30 mb_30">Volunteer</a>&nbsp;&nbsp;</div>
 						</li>
 						<li>
-						<div class="text-center"><a href="#" class="thm-btn mt_30 mb_30">Volunteer</a></div>
-                        </li>
+						<div class="text-center"><a href="#" class="thm-btn mt_30 mb_30">Donate</a>&nbsp;&nbsp;</div>
+						</li>
 					</ul>
 				</div>
 				<div class="nav-footer pull-right">
